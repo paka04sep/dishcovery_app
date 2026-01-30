a63798d9e7ac43dc4d885995e2a12033f77140c4
paka04sep <pakawat.20074@gmail.com>
2026-01-30 12:12:07 +0700

    firebase options

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'starting_screen/auth_wrapper.dart';
-// import 'firebase_options.dart'; // Uncomment if using generated options
+import 'firebase_options.dart'; // Uncomment if using generated options
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    // options: DefaultFirebaseOptions.currentPlatform, // Uncomment if using generated options
+    options: DefaultFirebaseOptions
+        .currentPlatform, // Uncomment if using generated options
   );
   runApp(const MyApp());
 }
